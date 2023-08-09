08cb99da5470ce84dfd77a59bdc12b442770d4a3
Vincent Mabasa <vincent.mabasa@multichoice.com>
2023-08-09 10:34:49 +0200

    ensuring contect loads

---
+++ src/test/java/com/vincent/assessment/VendingMachineApplicationTest.java
@@ -0,0 +1,11 @@
+package com.vincent.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VendingMachineApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
